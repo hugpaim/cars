edd9e05a24d35ac2408096a2f187dd41e9162cc4
hugpaim <102909728+hugpaim@users.noreply.github.com>
2022-09-01 21:08:27 -0300

    simple react app with destructuring

---
+++ src/conponents/app.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import cars from "../pratices";
+
+export default function App() {
+
+    const [honda, tesla] = cars;
+    const {speedStats:{topSpeed:hondaTopSpeed}} = honda;
+    const {speedStats:{topSpeed:teslaTopSpeed}} = tesla;
+
+    const {coloursByPopularity:[hondaTopColour]} = honda
+    const {coloursByPopularity:[teslaTopColour]} = tesla
+
+    return (
+    <table>
+     <tr>
+       <th>Brand</th>
+      <th>Top Speed</th>
+     </tr>
+    <tr>
+      <td>{tesla.model}</td>
+       <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+     </tr>
+     <tr>
+       <td>{honda.model}</td>
+       <td>{hondaTopSpeed}</td>
+       <td>{hondaTopColour}</td>
+     </tr>
+   </table>
+    )
+  }
